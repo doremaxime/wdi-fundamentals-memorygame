867abb8af63067983a2324fb5f16acfd49e700c2
Max Doré <max@RogerMilersiMac.T-mobile.com>
2016-12-23 10:15:50 -0500

    added java if/else

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,21 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("js file connected");
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+	if (cardOne === cardTwo) {
+		alert('You found a match!');
+	}
+	else {
+		alert('Sorry, try again!');
+	}
+
+	if (cardThree === cardFour) {
+		alert('You found a match!');
+	}
+	else {
+		alert('Sorry, try again!');
+	}
