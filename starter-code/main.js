7abc36983d0bbf6789dd357ef33cce11b263d63c
Max Doré <max@RogerMilersiMac.T-mobile.com>
2017-01-05 16:02:50 -0500

    finished up js. could not get the .board div in css to stay perfectly in the middle

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,40 @@
-console.log("js file connected");
-
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+var cards =["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var gameBoard = document.getElementById('game-board');
 
+var createBoard = function(){
+	for(var i = 0; i <= 3; i++){
+	var newCard = document.createElement('div');  
+	newCard.className = 'card';  
+	newCard.setAttribute('data-card', cards[i]);
+	newCard.addEventListener('click', isTwoCards);
+	gameBoard.appendChild(newCard);
+	}
+};
 
-	if (cardOne === cardTwo) {
-		alert('You found a match!');
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = '<img src="/Users/max/fundamentals/wdi-fundamentals-memorygame/starter-code/my_king.png" width="150px" alt="King of Spades">';
+	} else { this.innerHTML = '<img src="/Users/max/fundamentals/wdi-fundamentals-memorygame/starter-code/my_queen.png" width="150px" alt="Queen of Clubs">';
 	}
-	else {
-		alert('Sorry, try again!');
+  	if (cardsInPlay.length === 2) {
+    	isMatch(cardsInPlay);
+		cardsInPlay = [];
 	}
+};
 
-	if (cardThree === cardFour) {
-		alert('You found a match!');
-	}
-	else {
-		alert('Sorry, try again!');
-	}
+function isMatch(cards) {
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+    	alert("You found a match!");
+ 	} else {
+    	alert("Sorry, try again.");
+ 	}
+};
+
+createBoard();
