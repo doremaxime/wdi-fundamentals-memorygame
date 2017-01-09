dc735d19a907f5585295321118164475cd3b5fd8
Maxime Dore <dore.maxime@gmail.com>
2017-01-08 22:08:23 -0500

    Added the page refresher

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,11 +8,11 @@ var gameBoard = document.getElementById('game-board');
 
 var createBoard = function(){
 	for(var i = 0; i <= 3; i++){
-	var newCard = document.createElement('div');  
-	newCard.className = 'card';  
-	newCard.setAttribute('data-card', cards[i]);
-	newCard.addEventListener('click', isTwoCards);
-	gameBoard.appendChild(newCard);
+	    var newCard = document.createElement('div');  
+	    newCard.className = 'card';  
+	    newCard.setAttribute('data-card', cards[i]);
+	    newCard.addEventListener('click', isTwoCards);
+	    gameBoard.appendChild(newCard);
 	}
 };
 
@@ -21,20 +21,31 @@ var isTwoCards = function() {
 	console.log(this.getAttribute('data-card'));
 	if (this.getAttribute('data-card') === 'king') {
 		this.innerHTML = '<img src="/Users/max/fundamentals/wdi-fundamentals-memorygame/starter-code/my_king.png" width="150px" alt="King of Spades">';
-	} else { this.innerHTML = '<img src="/Users/max/fundamentals/wdi-fundamentals-memorygame/starter-code/my_queen.png" width="150px" alt="Queen of Clubs">';
-	}
+		} else { 
+        this.innerHTML = '<img src="/Users/max/fundamentals/wdi-fundamentals-memorygame/starter-code/my_queen.png" width="150px" alt="Queen of Clubs">';
+        }
+
   	if (cardsInPlay.length === 2) {
     	isMatch(cardsInPlay);
 		cardsInPlay = [];
 	}
 };
 
-function isMatch(cards) {
- 	if (cardsInPlay[0] === cardsInPlay[1]) {
-    	alert("You found a match!");
- 	} else {
-    	alert("Sorry, try again.");
- 	}
+function refresh() {
+    setTimeout(function () {
+        location.reload()
+    }, 800);
 };
+
+function isMatch(cards) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+        } else {
+        alert("Sorry, try again.");
+    }
+    refresh();
+};
+
+
 
 createBoard();
